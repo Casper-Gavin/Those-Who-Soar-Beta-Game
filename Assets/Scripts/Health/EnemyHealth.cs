d240dcf644a5eee87326f37d536c4f3689e8c538
carterawebb <carterwebb@nevada.unr.edu>
2024-01-27 13:33:23 -0800

    Melee Bomber, not working

--- Assets/Scripts/Health/EnemyHealth.cs
+++ Assets/Scripts/Health/EnemyHealth.cs
@@ -11,6 +11,18 @@ public class EnemyHealth : HealthBase
     private Image healthBarImage;
     private GameObject gameObjectHealthBar;
 
+    [SerializeField] private Color flashColor = Color.white;
+    [SerializeField] private float flashDuration = 0.5f;
+    [SerializeField] private int numberOfFlashes = 4;
+    [SerializeField] private float explosionRadius = 5f;
+    [SerializeField] public float explosionDamage = 5f;
+    [SerializeField] private GameObject explosionEffectPrefab;
+
+    private SpriteRenderer spriteRenderer;
+    private Color originalColor;
+    private int currentFlashCount = 0;
+    public bool IsDead;
+
     protected override void Awake() // this used to be Start(), it is apparently very weird to use Start() with MonoBehavior inheritance
     {
         // Instantiate is used to create game objects (prefabs) on the fly
@@ -18,6 +30,11 @@ public class EnemyHealth : HealthBase
         gameObjectHealthBar.transform.parent = transform;
         healthBarImage = gameObjectHealthBar.transform.GetChild(0).transform.GetChild(0).GetComponent<Image>();
         CurrentHealth = initialHealth;
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        originalColor = spriteRenderer.color;
+
+        IsDead = false;
     }
 
     protected override void Update()
@@ -36,5 +53,87 @@ public class EnemyHealth : HealthBase
     protected override void UpdateHealth()
     {
         healthBarImage.fillAmount = Mathf.Lerp(healthBarImage.fillAmount, CurrentHealth / maxHealth, 10f * Time.deltaTime);
+    }
+
+    protected override void Die()
+    {
+        if (gameObject.CompareTag("Bomber"))
+        {
+            IsDead = true;
+            TriggerFlash();
+        }
+        else
+        {
+            DestroyObject();
+        }
+    }
+
+    private void TriggerFlash() 
+    {
+        if (currentFlashCount < numberOfFlashes)
+        {
+            StartCoroutine(Flash());
+        }
+        else
+        {
+            spriteRenderer.color = originalColor;
+            
+            Explode();
+        }
+    }
+
+    private IEnumerator Flash()
+    {
+        float lerpTime = flashDuration / 2;
+        float time = 0;
+
+        while (time < lerpTime)
+        {
+            spriteRenderer.color = Color.Lerp(originalColor, flashColor, time / lerpTime);
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        spriteRenderer.color = flashColor;
+        time = 0;
+
+        while (time < lerpTime)
+        {
+            spriteRenderer.color = Color.Lerp(flashColor, originalColor, time / lerpTime);
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        spriteRenderer.color = originalColor;
+        currentFlashCount++;
+        TriggerFlash();
+    }
+
+    private void Explode()
+    {
+        // Instantiate the explosion effect
+        if (explosionEffectPrefab)
+        {
+            Instantiate(explosionEffectPrefab, transform.position, Quaternion.identity);
+        }
+
+        // Find all colliders within the explosion radius
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, explosionRadius);
+        foreach (var collider in colliders)
+        {
+            if (collider.CompareTag("Player"))
+            {
+                collider.GetComponent<PlayerHealth>().TakeDamage((int)explosionDamage);
+            }
+        }
+
+        DestroyObject();
+    }
+
+    void OnDrawGizmos()
+    {
+        // Draw a red sphere in the editor to visualize explosion radius
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
     }
 }
--- Assets/Scripts/Health/PlayerDamage.cs
+++ Assets/Scripts/Health/PlayerDamage.cs
@@ -8,9 +8,11 @@ public class PlayerDamage : MonoBehaviour
     [SerializeField] private int damageToApply = 1;
 
     private PlayerHealth playerHealth;
+    //private EnemyHealth enemyHealth;
     private void Start()
     {
         playerHealth = GetComponent<PlayerHealth>();
+        //enemyHealth = GetComponent<EnemyHealth>();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -21,6 +23,10 @@ public class PlayerDamage : MonoBehaviour
             {
                 playerHealth.TakeDamage(damageToApply);
             }
-        }
+        } /*
+        else if (other.CompareTag("Bomber") && enemyHealth.IsDead == true)
+        {
+            playerHealth.TakeDamage((int)enemyHealth.explosionDamage);
+        }*/
     }
 }
