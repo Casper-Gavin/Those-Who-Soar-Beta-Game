3ed29f41d5c697a516418b3b8745e59c08f3f6f0
Gavin Casper <gcasper@nevada.unr.edu>
2024-03-02 11:03:20 -0800

    Refill Test

--- Assets/Tests/PlayTests/WeaponAmmoTest.cs
+++ Assets/Tests/PlayTests/WeaponAmmoTest.cs
@@ -7,14 +7,27 @@ using UnityEditor;
 
 public class WeaponAmmoTest
 {
+    // [Test]
+    // public void TestConsumeAmmo()
+    // {
+    //     GameObject gun = GameObject.Instantiate(AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefabs/Weapons/PlayerWeapons/Weapon_Special.prefab"), 
+    //                                  new Vector3(0, 0, 0), 
+    //                                  Quaternion.identity);
+    //     gun.GetComponent<WeaponAim>().InjectPlayer = true;
+    //     gun.GetComponent<WeaponAmmo>().ConsumeAmmo();
+    //     Assert.AreEqual(gun.GetComponent<WeaponAmmo>().GetComponent<ProjectileWeapon>().CurrentAmmo, 29);
+    // }
+
     [Test]
-    public void TestConsumeAmmo()
+    public void TestRefillAmmo()
     {
-        GameObject gun = GameObject.Instantiate(AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefabs/Weapons/PlayerWeapons/Weapon_Special.prefab"), 
-                                     new Vector3(0, 0, 0), 
-                                     Quaternion.identity);
+        GameObject gun = GameObject.Instantiate(AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefabs/Weapons/PlayerWeapons/Weapon_Special.prefab"), new Vector3(0, 0, 0), Quaternion.identity);
         gun.GetComponent<WeaponAim>().InjectPlayer = true;
+
+        //shoots then refills the ammo
         gun.GetComponent<WeaponAmmo>().ConsumeAmmo();
-        Assert.AreEqual(gun.GetComponent<WeaponAmmo>().GetComponent<ProjectileWeapon>().CurrentAmmo, 29);
+        gun.GetComponent<WeaponAmmo>().RefillAmmo();
+
+        Assert.AreEqual(gun.GetComponent<WeaponAmmo>().GetComponent<ProjectileWeapon>().CurrentAmmo, 30);
     }
 }
